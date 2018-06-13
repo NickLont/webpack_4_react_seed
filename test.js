c008f137db1938de26a8a163c1ae698f9752c3df
nicklontorfos <nlontorfos@hotmail.com>
2018-06-13 13:00:25 +0200

    testing prettier

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-import React, {Component } from 'react'
+import React, { Component } from "react"
 
-class Class extends Component   {
+class Class extends Component {
   render() {
-    return  (
-      <div> </div>
-    )
+    return <div> </div>
   }
 }
 
