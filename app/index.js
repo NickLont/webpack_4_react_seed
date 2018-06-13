09c34c9ffb3fccaf3e571266f132803da5085edd
nicklontorfos <nlontorfos@hotmail.com>
2018-06-13 12:55:34 +0200

    testing prettier

--- app/index.js
+++ app/index.js
@@ -4,9 +4,7 @@ import "./assets/sass/style.scss"
 
 class App extends Component {
   render() {
-    return
-
-    <p className="index-page__title">React index page</p>
+    return <p className="index-page__title">React index page</p>
   }
 }
 
