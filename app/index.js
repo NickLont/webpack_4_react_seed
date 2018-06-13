e4ea245d021a138da8bcf3c42f41a4a6aa1979c5
nicklontorfos <nlontorfos@hotmail.com>
2018-06-13 12:50:05 +0200

    testing prettier

--- app/index.js
+++ app/index.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import './assets/sass/style.scss'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/sass/style.scss";
 
 class App extends Component {
-  render () {
-    return (
-      <p className="index-page__title">React index page</p>
-    )
+  render() {
+    return <p className="index-page__title">React index page</p>;
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
