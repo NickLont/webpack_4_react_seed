0f63b0f6e11cfbaa6f1ca41497deb90d369184e5
nicklontorfos <nlontorfos@hotmail.com>
2018-10-26 17:25:15 +0200

    dependencies updated to last version, eslint works

--- app/containers/pages/homepage/index.js
+++ app/containers/pages/homepage/index.js
@@ -3,11 +3,12 @@ import Button from "react-bootstrap/lib/Button"
 
 class Homepage extends Component {
   render() {
-    // console.log("env is:", process.env)
+    console.log("env is:", process.env)
     return (
       <div className="container homepage">
         <h3 className="homepage__title">App Homepage</h3>
         <Button>Bootstrap Button</Button>
+        {["asdad", "asds"]}
       </div>
     )
   }
