dc719992878b73b7ee35a41c0326c6a84a375d47
Nick Lontorfos <nick@touchtribe.nl>
2019-07-12 16:41:19 +0200

    test

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -8,14 +8,15 @@ const initialState  = Map({
 
 const UserReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'FETCH_USER_REQUEST' :
-      return state.set('loading', true)
-    case 'FETCH_USER_SUCCESS' :
-      return state.set('loading', false).set('data', action.data)
-    case 'FETCH_USER_FAILURE' :
-      return state.set('loading', false).set('error', action.error)
-    default:
-      return state
+  case 'FETCH_USER_REQUEST' :
+    return state.set('loading', true)
+  case 'FETCH_USER_SUCCESS' :
+    return state.set('loading', false).set('data', action.data)
+  case 'FETCH_USER_FAILURE' :
+    return state.set('loading', false).set('error', action.error)
+  default:
+    return state
+    
   }
 }
 
